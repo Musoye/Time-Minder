d9b6659b918c2f16b91ea4d39425c216215db804
Musoye <oyebamijimustapha44@gmail.com>
2023-06-03 07:43:46 +0100

    feat: update the backend code, implement the projrct, project edittion , task and task edition

--- backend/app.py
+++ backend/app.py
@@ -7,6 +7,8 @@ import json
 
 app = Flask(__name__)
 app.secret_key = "I'llneverbebrokeinmylifeandI'macomingchampion"
+url_1 = 'http://127.0.0.1:5001/api'
+url_2 = 'http://127.0.0.1:5002/notify'
 
 @app.route('/auth/signup')
 def signup():
@@ -18,7 +20,7 @@ def login():
     """The page for the login"""
     return render_template('index.html')
 
-@app.route('/signup')
+@app.route('/signup', methods=['POST'])
 def auth_signup():
     """The signup handler"""
     if request.method == 'POST':
@@ -28,9 +30,9 @@ def auth_signup():
         new_user['last_name'] = entry.get('lastname', None)
         new_user['password'] = entry.get('password')
         new_user['email'] = entry.get('email')
-        result = req.post('http://127.0.0.1:5001/api/users', json=new_user)
-        resul = req.post('http://127.0.0.1:5002/notify/register', json=new_user)
-        if result.status_code == 201 or resul.status_code == 201:
+        result = req.post('{}/users'.format(url_1), json=new_user)
+        resul = req.post('{}/register'.format(url_2), json=new_user)
+        if result.status_code == 201:
             return redirect(url_for('login'))
         else:
             return redirect(url_for('signup'))
@@ -45,11 +47,13 @@ def auth_login():
         username = entry['username']
         password = entry['password']
         password = md5(password.encode()).hexdigest().lower()
-        result = req.get('http://127.0.0.1:5001/api/users').json()
+        result = req.get('{}/users'.format(url_1)).json()
         for user in result:
             if user.get('email') == username:
                 if user.get('password') == password:
                     session['user'] = user.get('id')
+                    session['email'] = user.get('email')
+                    session['first'] = user.get('first_name')
                 return redirect(url_for('dashboard'))
     return redirect(url_for('signup'))
 
@@ -61,15 +65,13 @@ def change_password_page():
 @app.route('/profile/change_password', methods=['POST'])
 def change_password():
     """Handler for changing of password"""
-    print('am I welcome')
     if request.method == 'POST':
-        print('am always welcome')
         entry = request.form
         email = entry.get('email')
         user_id = None
         password = entry.get('password')
         password = md5(password.encode()).hexdigest().lower()
-        result = req.get('http://127.0.0.1:5001/api/users').json()
+        result = req.get('{}/users'.format(url_1)).json()
         for user in result:
             if user.get('email') == email:
                 user_id = user.get('id')
@@ -83,15 +85,27 @@ def change_password():
             'email': email,
             'password': password
         }
-        resul = req.post('http://127.0.0.1:5002/notify/password', json=data)
-        print(resul.status_code)
+        resul = req.post('{}/password'.format(url_2), json=data)
         if resul.status_code in [200, 201]:
             return 'You password has been updated successfully'
         else:
-            print('The email cannot be sent')
             return redirect(url_for('signup'))
-    print('The problem is here')
     return redirect(url_for('signup'))
+
+@app.route('/profile/update', method=['POST'])
+def update_user():
+    if 'user' in session:
+        uid = session.get('user')
+        entry = request.form
+        new_values = {}
+        new_values['first_name'] = entry['first_name']
+        new_values['last_name'] = entry['last_name']
+        result = req.put('{}/users/{}'.format(url_1, uid), json=new_values)
+        if result.status_code == 200:
+            return redirect(url_for('dashboard'))
+        else:
+            return redirect(url_for('update_user'))
+    return redirect(url_for('login'))
 
 @app.route('/dashboard')
 def dashboard():
@@ -100,6 +114,93 @@ def dashboard():
         user_id = session['user']
         return 'Dashboard of Life by Musoye of user {}'.format(user_id)
     return redirect(url_for('signup'))
+
+@app.route('/project/create', methods=['POST'])
+def project_creation():
+    """Creation of a new project"""
+    if 'user' in session:
+        uid = session.get('user')
+        if request.method == 'POST':
+            entry = request.form
+            new_pro = {}
+            new_pro['name'] = entry.get('name')
+            new_pro['description'] = entry.get('description')
+            new_pro['expiry_date'] = entry.get('expiry')
+            new_pro['email'] = session.get('email')
+            new_pro['user_id'] = uid
+            result = req.post('{}/projects'.format(url_1), json=new_pro)
+            resul = req.post('{}/project'.format(url_2), json=new_pro)
+            if result.status_code == 201:
+                return redirect(url_for('dashboard'))
+            else:
+                return redirect(url_for('login'))
+    return redirect(url_for('login'))
+
+@app.route('/<project_id>/edit', methods=['POST'])
+def project_edition(project_id):
+    """Edition of a project"""
+    prd = project_id
+    if 'user' in session:
+        uid = session.get('user')
+        if request.method == 'POST':
+            entry = request.form
+            new_pro = {}
+            new_pro['name'] = entry.get('name')
+            new_pro['description'] = entry.get('description')
+            new_pro['expiry_date'] = entry.get('expiry')
+            new_pro['user_id'] = uid
+            result = req.put('{}/projects/{}'.format(url_1, prd), json=new_pro)
+            if result.status_code == 201:
+                return redirect(url_for('dashboard'))
+            else:
+                return redirect(url_for('login'))
+    return redirect(url_for('login'))
+
+@app.route('/<project_id>/task/create', methods=['POST'])
+def task_creation(project_id):
+    """Creation of a new task"""
+    prd = project_id
+    if 'user' in session:
+        uid = session.get('user')
+        if request.method == 'POST':
+            entry = request.form
+            new_pro = {}
+            new_pro['name'] = entry.get('name')
+            new_pro['description'] = entry.get('description')
+            new_pro['expiry_date'] = entry.get('expiry')
+            new_pro['email'] = session.get('email')
+            new_pro['user_id'] = uid
+            new_pro['status'] = entry.get('status')
+            new_pro['project_name'] = prd
+            new_pro['project_id'] = prd
+            result = req.post('{}/tasks'.format(url_1), json=new_pro)
+            resul = req.post('{}/task'.format(url_2), json=new_pro)
+            if result.status_code == 201:
+                return redirect(url_for('dashboard'))
+            else:
+                return redirect(url_for('login'))
+    return redirect(url_for('login'))
+
+@app.route('/project/<task_id>/edit', methods=['POST'])
+def task_edition(task_id):
+    """Edition of a Task"""
+    tad = task_id
+    if 'user' in session:
+        uid = session.get('user')
+        if request.method == 'POST':
+            entry = request.form
+            new_pro = {}
+            new_pro['name'] = entry.get('name')
+            new_pro['description'] = entry.get('description')
+            new_pro['expiry_date'] = entry.get('expiry')
+            new_pro['user_id'] = uid
+            new_pro['status'] = entry.get('status')
+            result = req.put('{}/tasks/{}'.format(url_1, tad), json=new_pro)
+            if result.status_code == 201:
+                return redirect(url_for('dashboard'))
+            else:
+                return redirect(url_for('login'))
+    return redirect(url_for('login'))
 
 @app.route('/logout')
 def sign_out():
